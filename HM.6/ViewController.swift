da59b387c559c00239085c4b1cd2ccc68170ecfc
Hro-07 <hrostepanyan5@gmail.com>
2023-10-06 14:56:50 +0200

    Initial Commit

---
+++ HM.6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HM.6
+//
+//  Created by Hrayr on 6/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
